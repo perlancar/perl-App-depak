10d547c551f4f715c9f092e378beb9e94f9c53f4
Steven Haryanto (on PC, Jakarta) <stevenharyanto@gmail.com>
2014-04-11 17:38:05 +0700

    Use tracepm, change some options and option defaults (--include-modules -> --include, --exclude-modules -> --exclude, --exclude-module-patterns -> --exclude-pattern, add --method, remove --use-{prereq-scanner,fatpack-trace}, remove --skip-not-found)

--- lib/App/fatten.pm
+++ lib/App/fatten.pm
@@ -7,6 +7,7 @@ use experimental 'smartmatch';
 use Log::Any '$log';
 BEGIN { no warnings; $main::Log_Level = 'info' }
 
+use App::tracepm;
 use Cwd qw(abs_path);
 use File::chdir;
 use File::Copy;
@@ -15,10 +16,8 @@ use File::Slurp::Shortcuts qw(slurp slurp_c write_file);
 use File::Temp qw(tempfile tempdir);
 use List::Util qw(first);
 use Log::Any::For::Builtins qw(system my_qx);
-use Module::CoreList;
 use Module::Path qw(module_path);
 #use SHARYANTO::Dist::Util qw(packlist_for);
-use SHARYANTO::Module::Util qw(is_xs);
 use SHARYANTO::Proc::ChildError qw(explain_child_error);
 use String::ShellQuote;
 use version;
@@ -29,108 +28,22 @@ sub _sq { shell_quote($_[0]) }
 
 our %SPEC;
 
-# add module to list to be included, return 1 if module is added the first time.
-sub _consider_module {
-    my ($self, $mod) = @_;
+sub _trace {
+    my $self = shift;
 
-    my $sm = $self->{seen_mods};
+    my $res = App::tracepm::tracepm(
+        method => $self->{trace_method},
+        script => $self->{input_file},
+        use => $self->{use},
+        recurse_exclude_core => 1,
+        recurse_exclude_xs   => 1,
+        detail => 1,
 
-    return 0 if exists $sm->{$mod};
-
-    if ($self->{skip_not_found} && !module_path($mod)) {
-        $sm->{$mod} = 0;
-        $log->warn("  Skipped: $mod (not found)");
-        return 0;
-    }
-    if ($mod !~ /\.pm\z/) {
-        $sm->{$mod} = 0;
-        $log->warn("  Skipped: $mod (not .pm file)");
-        return 0;
-    }
-    if ($mod ~~ @{ $self->{exclude_modules} }) {
-        $sm->{$mod} = 0;
-        $log->warn("  Skipped: $mod (excluded)");
-        return 0;
-    }
-    for (@{ $self->{exclude_module_patterns} }) {
-        if ($mod ~~ $_) {
-            $sm->{$mod} = 0;
-            $log->warn("  Skipped: $mod (excluded $_)");
-            return 0;
-        }
-    }
-    my $pm = $mod; $pm =~ s!/!::!g; $pm =~ s/\.pm\z//;
-    my $is_core = Module::CoreList::is_core($pm, undef, $self->{perl_version});
-    if ($is_core && !($mod ~~ @{ $self->{include_modules} })) {
-        $sm->{$mod} = 0;
-        $log->warn("  Skipped: $mod (core)");
-        return 0;
-    }
-    if (is_xs($mod)) {
-        $sm->{$mod} = 0;
-        $log->warn("  Skipped: $mod (XS)");
-        return 0;
-    }
-    $log->info("  Added: $mod");
-    $sm->{$mod} = 1;
-    1;
-}
-
-sub _trace_with_fatpack_trace {
-    my ($self, $path) = @_;
-
-    $self->{_seen_paths} //= {};
-    return if $self->{_seen_paths}{$path}++;
-
-    $log->debug("  Analyzing $path with 'fatpack trace' ...");
-
-    my $tempdir = $self->{tempdir};
-
-    my ($tracefh, $tracef) = tempfile("fatpack.trace.XXXXXX", DIR=>$tempdir);
-    system("fatpack", "trace", "--to", $tracef,
-           (map {("--use", $_)} @{ $self->{use} // [] }), $path);
-    die "Can't fatpack trace: ".explain_child_error() if $?;
-
-    # i'm not having much success using the 'fatpack packlists-for' and 'fatpack
-    # tree' commands. so i'm adding the .pm files directly here for now, and
-    # later finally using 'fatpack file'.
-
-    my @new_mods;
-
-    open my($fh), "<", $tracef or die "Can't open $tracef: $!";
-    while (my $mod = <$fh>) {
-        chomp($mod);
-        push @new_mods, $mod if $self->_consider_module($mod);
-    }
-
-    #my @paths = grep {$_} map { module_path($_) } sort @new_mods;
-    #$self->_trace_with_fatpack_trace($_) for @paths;
-}
-
-sub _trace_with_prereq_scanner {
-    my ($self, $path) = @_;
-
-    $self->{_seen_paths} //= {};
-    return if $self->{_seen_paths}{$path}++;
-
-    $log->debug("  Analyzing $path with PrereqScanner ...");
-
-    state $scanner = do {
-        require Perl::PrereqScanner;
-        Perl::PrereqScanner->new;
-    };
-
-    my @new_mods;
-    my $res = $scanner->scan_file($path)->as_string_hash;
-    for my $m0 (keys %$res) {
-        next if $m0 =~ /\A(perl)\z/;
-
-        my $m = $m0; $m =~ s!::!/!g; $m .= ".pm";
-        push @new_mods, $m if $self->_consider_module($m);
-    }
-
-    my @paths = grep {$_} map { module_path($_) } @new_mods;
-    $self->_trace_with_prereq_scanner($_) for @paths;
+        core => 0,
+        xs   => 0,
+    );
+    die "Can't trace: $res->[0] - $res->[1]" unless $res->[0] == 200;
+    $self->{deps} = $res->[2];
 }
 
 sub _build_lib {
@@ -143,13 +56,40 @@ sub _build_lib {
 
     local $CWD = "$tempdir/lib";
 
-    my $sm = $self->{seen_mods};
-    for my $mod (sort(grep {$sm->{$_}} keys %$sm)) {
+    my @mods; # modules to add
+
+    my $deps = $self->{deps};
+    push @mods, (map {$_->{module}} grep {!$_->{is_core} && !$_->{is_xs}} @$deps);
+
+    push @mods, @{ $self->{include} // [] };
+
+    # filter excluded
+    my @fmods;
+  MOD:
+    for my $mod (@mods) {
+        if ($self->{exclude} && $mod ~~ @{ $self->{exclude} }) {
+            $log->infof("Excluding %s: skipped", $mod);
+            next;
+        }
+        for (@{ $self->{exclude_pattern} // [] }) {
+            if ($mod ~~ /$_/) {
+                $log->infof("Excluding %s: skipped by pattern %s", $mod, $_);
+                next MOD;
+            }
+        }
+        push @fmods, $mod;
+    }
+    @mods = @fmods;
+
+    for my $mod (@mods) {
         my $mpath = module_path($mod) or die "Can't find path for $mod";
-        my ($dir) = $mod =~ m!(.+)/(.+)!;
+
+        my $modp = $mod; $modp =~ s!::!/!g; $modp .= ".pm";
+        my ($dir) = $modp =~ m!(.+)/(.+)!;
         if ($dir) {
             make_path($dir) unless -d $dir;
         }
+
         if ($self->{strip}) {
             state $stripper = do {
                 require Perl::Stripper;
@@ -161,10 +101,11 @@ sub _build_lib {
             write_file($mod, $stripped);
         } else {
             $log->debug("  Copying $mpath --> $mod ...");
-            copy($mpath, $mod);
+            copy($mpath, $modp);
         }
+
         $totfiles++;
-        $totsize += (-s $mod);
+        $totsize += (-s $mpath);
     }
     $log->infof("  Added %d files (%.1f KB)", $totfiles, $totsize/1024);
 }
@@ -202,38 +143,36 @@ $SPEC{fatten} = {
             cmdline_aliases => { i=>{} },
         },
         output_file => {
-            summary => 'Path to output file, defaults to INPUT.packed',
+            summary => 'Path to output file, defaults to `packed` in current directory',
             schema => ['str*'],
             cmdline_aliases => { o=>{} },
         },
-        include_modules => {
+        include => {
             summary => 'Modules to include',
             description => <<'_',
 
 When the tracing process fails to include a required module, you can add it
-here. Either specify in the form of `Package::SubPkg` or `Package/SubPkg.pm`.
+here.
 
 _
             schema => ['array*' => of => 'str*'],
             cmdline_aliases => { I => {} },
         },
-        exclude_modules => {
+        exclude => {
             summary => 'Modules to exclude',
             description => <<'_',
 
-When you don't want to include a module, specify it here. Either specify in the
-form of `Package::SubPkg` or `Package/SubPkg.pm`.
+When you don't want to include a module, specify it here.
 
 _
             schema => ['array*' => of => 'str*'],
             cmdline_aliases => { E => {} },
         },
-        exclude_module_patterns => {
+        exclude_pattern => {
             summary => 'Regex patterns of modules to exclude',
             description => <<'_',
 
-When you don't want to include a pattern of modules, specify it here. The regex
-will be matched against module name in the form `Package/SubPkg.pm`.
+When you don't want to include a pattern of modules, specify it here.
 
 _
             schema => ['array*' => of => 'str*'],
@@ -248,12 +187,13 @@ _
         #    schema => [bool => default => 0],
         #    summary => 'Whether to overwrite output if previously exists',
         #},
-        use_fatpack_trace => {
-            summary => "Whether to use 'fatpack trace' to detect required modules",
-            schema => ['bool', default=>1],
+        trace_method => {
+            summary => "Which method to use to trace dependencies",
+            schema => ['str*', default => 'fatpacker'],
             description => <<'_',
 
-`fatpack trace` uses `perl -c`.
+The default is `fatpacker`, which is the same as what `fatpack trace` does.
+There are other methods available, please see `App::tracepm` for more details.
 
 _
         },
@@ -262,37 +202,8 @@ _
             schema => ['array*' => of => 'str*'],
             description => <<'_',
 
-Will be passed to `fatpack trace`'s `--use` option.
-
-_
-        },
-        use_prereq_scanner => {
-            summary => 'Whether to use Perl::PrereqScanner to detect required modules',
-            schema => ['bool', default=>0],
-            description => <<'_',
-
-Since fatpack uses `perl -c`, it might miss a few modules. You can add more
-modules manually via `include_modules` (or `use`) or let Perl::PrereqScanner
-does a static analysis of script/module source code. This on the other hand is
-usually too aggressive, as it will include modules in cases like this:
-
-    if ($some_feature_or_os) {
-        require Win32::Console::ANSI;
-    }
-
-and you'll need to exclude some modules from being recursively searched using
-`exclude_modules` or `exclude_module_patterns`.
-
-_
-            cmdline_aliases => { V=>{} },
-        },
-        skip_not_found => {
-            summary => 'Instead of dying, skip modules that are not found',
-            schema => 'bool',
-            description => <<'_',
-
-Unless you explicitly set it, this option is automatically turned on when you
-use `use_prereq_scanner`.
+Will be passed to the tracer. Will currently only affect the `fatpacker` and
+`require` methods (because those methods actually run your script).
 
 _
         },
@@ -315,23 +226,6 @@ sub fatten {
     $log->debugf("Created tempdir %s", $tempdir);
     $self->{tempdir} = $tempdir;
 
-    $self->{skip_not_found} //= 1 if $self->{use_prereq_scanner};
-
-    $self->{include_modules} //= [];
-    $self->{exclude_modules} //= [];
-    for my $list ($self->{include_modules}, $self->{exclude_modules}) {
-        for my $mod (@$list) {
-            unless ($mod =~ /\.pm\z/) {
-                $mod =~ s!::!/!g; $mod .= ".pm";
-            }
-        }
-    }
-
-    $self->{exclude_module_patterns} //= [];
-    for (@{ $self->{exclude_module_patterns} }) {
-        $_ = qr/$_/;
-    }
-
     # my understanding is that fatlib contains the stuffs beside the pure-perl
     # .pm files, and currently won't pack anyway.
     #mkdir "$tempdir/fatlib";
@@ -345,34 +239,12 @@ sub fatten {
     $self->{abs_input_file} = abs_path($self->{input_file})
         or die "Can't find absolute path of input file $self->{input_file}";
 
-    $self->{output_file} //= "$self->{input_file}.packed";
+    $self->{output_file} //= "$CWD/packed";
     $self->{abs_output_file} = abs_path($self->{output_file})
         or die "Can't find absolute path of output file $self->{output_file}";
 
-    # list of modules that have been considered. key = Package/SubPkg.pm, val =
-    # 1 if to be added, 0 if to be excluded
-    $self->{seen_mods} = {};
-
-    $log->infof("Tracing modules to be included ...");
-
-    my @new;
-    for my $mod (@{ $self->{include_modules} }) {
-        push @new, $mod if $self->_consider_module($mod);
-    }
-    if ($self->{use_fatpack_trace}) {
-        my @paths = (
-            $self->{abs_input_file},
-            #(grep {$_} map {module_path($_)} sort @new),
-        );
-        $self->_trace_with_fatpack_trace($_) for @paths;
-    }
-    if ($self->{use_prereq_scanner}) {
-        my @paths = (
-            $self->{abs_input_file},
-            (grep {$_} map {module_path($_)} sort @new),
-        );
-        $self->_trace_with_prereq_scanner($_) for @paths;
-    }
+    $log->infof("Tracing dependencies ...");
+    $self->_trace;
 
     $log->infof("Building lib/ ...");
     $self->_build_lib;
